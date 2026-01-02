6948ea38415fd3f962626c2d3c276f84599f78a1
LindaPermata <permatalinda531@gmail.com>
2026-01-02 15:10:31 +0700

    Menambahkan ContainerApp dan DefaultContainerApp

--- app/src/main/java/com/example/questfirebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questfirebase/repositori/ContainerApp.kt
@@ -1,2 +1,13 @@
 package com.example.questfirebase.repositori
 
+import android.app.Application
+
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
