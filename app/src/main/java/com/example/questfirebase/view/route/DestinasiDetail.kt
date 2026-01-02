9b29f3b576e63bad944145e8684cbd9d77bc2712
LindaPermata <permatalinda531@gmail.com>
2026-01-02 15:21:25 +0700

    Menambahkan DestinasiDetail untuk navigasi detail siswa

--- app/src/main/java/com/example/questfirebase/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questfirebase/view/route/DestinasiDetail.kt
@@ -1,4 +1,11 @@
 package com.example.questfirebase.view.route
 
-object DestinasiDetail {
+import com.example.questfirebase.R
+import com.example.questfirebase.view.route.DestinasiNavigasi
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
