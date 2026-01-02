49b0d0d2f37a96f98ad8fdc30792c298782cf3ff
LindaPermata <permatalinda531@gmail.com>
2026-01-02 15:29:17 +0700

    Menambahkan code untuk destinasiedit

--- app/src/main/java/com/example/questfirebase/view/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questfirebase/view/route/DestinasiEdit.kt
@@ -1,4 +1,10 @@
 package com.example.questfirebase.view.route
 
-object DestinasiEdit {
+import com.example.questfirebase.R
+
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
