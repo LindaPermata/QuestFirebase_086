e5d926297dff20336ea81dae5615f67f53f061b2
LindaPermata <permatalinda531@gmail.com>
2026-01-08 11:12:25 +0700

    tambah factory HomeViewModel

--- app/src/main/java/com/example/questfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.questfirebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -14,5 +15,13 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        }
     }
 }
