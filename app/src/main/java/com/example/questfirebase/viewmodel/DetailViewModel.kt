4392081d274c7ccda1e8cb3ad96ac0139d195ab4
LindaPermata <permatalinda531@gmail.com>
2026-01-08 11:23:20 +0700

    tambah fungsi get satu siswa dengan status UI

--- app/src/main/java/com/example/questfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/DetailViewModel.kt
@@ -9,10 +9,42 @@ import androidx.lifecycle.viewModelScope
 import com.example.questfirebase.modeldata.Siswa
 import com.example.questfirebase.repositori.RepositorySiswa
 import com.example.questfirebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
 import java.io.IOException
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: Siswa?) : StatusUIDetail
     object Error : StatusUIDetail
     object loading : StatusUIDetail
+}
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa
+): ViewModel() {
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+    var statusUIDetail:StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa(){
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositorySiswa.getSatuSiswa(idSiswa) )
+            }
+            catch (e: IOException){
+                StatusUIDetail.Error
+            }
+            catch (e: Exception){
+                StatusUIDetail.Error
+            }
+        }
+    }
+
+
 }
