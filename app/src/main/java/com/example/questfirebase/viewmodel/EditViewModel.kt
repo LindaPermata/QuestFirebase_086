a5446428c7e65a0a0353ee8bed3cffc730fddebe
LindaPermata <permatalinda531@gmail.com>
2026-01-08 11:19:36 +0700

    tambah validasi input data siswa

--- app/src/main/java/com/example/questfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/EditViewModel.kt
@@ -32,3 +32,8 @@ RepositorySiswa
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
                 (detailSiswa))
     }
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
