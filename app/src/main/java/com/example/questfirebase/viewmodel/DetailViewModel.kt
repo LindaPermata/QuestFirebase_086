946f54ff0f8373d9eac1080e37d440d19c2ef775
LindaPermata <permatalinda531@gmail.com>
2026-01-08 11:22:39 +0700

    menambahkan status UI untuk halaman detail siswa

--- app/src/main/java/com/example/questfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/DetailViewModel.kt
@@ -1,1 +1,18 @@
 package com.example.questfirebase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase.modeldata.Siswa
+import com.example.questfirebase.repositori.RepositorySiswa
+import com.example.questfirebase.view.route.DestinasiDetail
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object loading : StatusUIDetail
+}
