9fb613f20fa92a8889fdf7f754244554b44d7a9a
LindaPermata <permatalinda531@gmail.com>
2026-01-08 11:12:39 +0700

    tambah factory Edit View Model

--- app/src/main/java/com/example/questfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/PenyediaViewModel.kt
@@ -21,7 +21,11 @@ object PenyediaViewModel {
                 aplikasiDataSiswa().container.repositorySiswa
             )
         }
-
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
         }
     }
 }
